4cf5d3eee8691b4a5eb8d17dbdfb4e5a1289fbad
Maximiliano Diaz <nebooz@gmail.com>
2016-08-01 14:06:13 -0400

    Removed the action bar

--- app/src/main/java/com/abnd/mdiaz/booklistingapp/MainActivity.java
+++ app/src/main/java/com/abnd/mdiaz/booklistingapp/MainActivity.java
@@ -44,8 +44,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        getSupportActionBar().setElevation(0);
     }
 
     public void bookSearch(View view) {
